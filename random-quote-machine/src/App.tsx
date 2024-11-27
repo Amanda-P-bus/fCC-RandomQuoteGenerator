1b8d27af2095c22374055075cf1b271830bd5e3f
Amanda Pollard <AmandaPhere@gmail.com>
2024-11-27 11:35:17 -0500

    groundwork

---
+++ random-quote-machine/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+interface Quote {
+  quote: string;
+  author: string;
+}
+
+const getRandomQuote = (): Quote => {return quotes}
+
+function App() {
+const [quote, setQuote] = useState<Quote>(getRandomQuote());
+
+  return "hello"
+}
+
+export default App
