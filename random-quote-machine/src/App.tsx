53d303160fc2ce1169130ea3b235ce4f32e24227
Amanda Pollard <AmandaPhere@gmail.com>
2024-11-27 15:56:47 -0500

    finalize design

--- random-quote-machine/src/App.tsx
+++ random-quote-machine/src/App.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
+import {FaTwitter, FaQuoteLeft, FaQuoteRight} from "react-icons/fa"
 
 import quotes from "./assets/quotes.json"
 
 import './App.css'
+import { BiAlignRight } from 'react-icons/bi';
 
 interface Quote {
   quote: string;
@@ -10,31 +12,51 @@ interface Quote {
 }
 
 
-
 const getRandomQuote = (): Quote => {return quotes[Math.floor(Math.random()*quotes.length)]}
 
+const getRandomColor = (): string =>
+  {const getR = Math.floor(Math.random()*150);
+   const getB = Math.floor(Math.random()*150);
+   const getG = Math.floor(Math.random()*150); 
+   return `rgb(${getR}, ${getB}, ${getG})`
+  }
+
 function App() {
+
 const [quote, setQuote] = useState<Quote>(getRandomQuote());
 
-  return <div>
+const [randomColor, setRandomColor] = useState<string>(getRandomColor());
+
+const changeQuote = () => {setQuote(getRandomQuote()); setRandomColor(getRandomColor())} ;
+
+
+  return   (
+  
+  <div id="mainDiv" className="background" style={{backgroundColor: randomColor}}>
+
+    <div id="mainTwo" >
+ 
     <h1>Random Quote Machine</h1>
     <div id="quote-box">
 
       <div id="catch-quote">
-        <div id="text">{quote.quote}</div>
-
-        <div id="author">{quote.author}</div>
-
+        <FaQuoteLeft size={30} style={{marginLeft: "3vh"}} />
+        <div id="text">{quote.quote}
+        </div>
+        
+        <FaQuoteRight size={30} style={{marginLeft:"68vh"}} /> 
+        
+        <div id="author" style={{backgroundColor: randomColor}}>-{quote.author}</div>
       </div>
-            
-      <button id="new-quote">New Quote</button>
-      
-      <button id="tweeter">
-        <a  id="tweet-quote" target="_blank" href="twitter.com/intent/tweet">Tweet Quote</a>
-      </button>
-      
+            <div id="holdBtns">
+      <button id="new-quote" onClick={changeQuote}>New Quote</button>
+        <button id="maybe">
+        <a  id="tweet-quote" target="_blank" href="twitter.com/intent/tweet"><FaTwitter style={{marginBottom: "-.5vh", paddingLeft: "1vh"}} /></a>
+        </button>  
+      </div>
+      </div>
     </div>
-  </div>
+  </div>)
 }
 
 export default App
